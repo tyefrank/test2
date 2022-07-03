aea0ef619ffec73733fc255208b5b41a807a9628
Tye Killough <killoughtye@gmail.com>
2022-07-03 16:46:07 -0500

    Modified Server File

--- server.js
+++ server.js
@@ -2,8 +2,60 @@ const express = require('express');
 require ('dotenv').config();
 const app = express();
 
-app.get("/", (req, res, next) => {
-    res.json({message: "Did you get this?"});
+app.get("/45", (req, res, next) => {
+    res.json({
+        message: "Using GET/45",
+        metadata: {
+            host: req.hostname,
+            port: process.env.PORT,
+            method: req.method,
+        }
+    });
+})
+
+app.get("/",(req, res, next) => {
+    res.json({
+        message: "Using GET/",
+        metadata: {
+            host: req.hostname,
+            port: process.env.PORT,
+            method: req.method,
+        }
+    });
+})
+
+// post, patch, delete
+app.post("/",(req, res, next) => {
+    res.json({
+        message: "Using POST/",
+        metadata: {
+            host: req.hostname,
+            port: process.env.PORT,
+            method: req.method,
+        }
+    });
+})
+
+app.patch("/89",(req, res, next) => {
+    res.json({
+        message: "Using PATCH/89",
+        metadata: {
+            host: req.hostname,
+            port: process.env.PORT,
+            method: req.method,
+        }
+    });
+})
+
+app.delete("/9",(req, res, next) => {
+    res.json({
+        message: "Using DELETE/9",
+        metadata: {
+            host: req.hostname,
+            port: process.env.PORT,
+            method: req.method,
+        }
+    });
 })
 
 
